27ce9a29686e582d84b4f6b1b662506dd32c1c5e
iagofontes <iagofontes@hotmail.com>
2019-04-03 21:55:00 -0300

    tela-inicio finalizada

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'snack-bar-message-component',
+  templateUrl: 'snack.bar.message.component.html',
+  styles: [`.color-message {
+    color: #03dac6;
+  }`],
+})
+export class SnackBarMessageComponent implements OnInit {
+  public message = '';
+  ngOnInit() {
+    this.message = sessionStorage.getItem('messageSnack').toString();
+  }
+}
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +23,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  @Input() clienteNome = '';
+
+  constructor(
+    public router: Router,
+    private snackBar: MatSnackBar
+    ) { }
 
   ngOnInit() {
   }
 
+  informarDestino() {
+    if (this.validarNome()) {
+      this.router.navigate(['/solicitarveiculo']);
+    } else {
+      this.showSnackBarWithMessage('Digite seu nome para continuar');
+    }
+  }
+
+  validarNome(): boolean {
+    if (this.clienteNome.length <= 0) {
+      return false;
+    }
+    return true;
+  }
+
+  showSnackBarWithMessage(message: string) {
+    sessionStorage.setItem('messageSnack', message);
+    this.snackBar.openFromComponent(SnackBarMessageComponent, {
+      duration: 5 * 1000,
+    });
+  }
 }
