fa71b0ffcee3c03eb6bcaa4c804b9e5c6b53e47f
Iago <iagofontes@hotmail.com>
2019-04-29 21:09:49 -0300

    correcao de serve.js e package.json

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
-// app.use(express.static(__dirname + '/dist/partiuapp'));
+app.use(express.static(__dirname + '/dist/partiu-app'));
 
 app.get('/*', function(req,res) {
-    // res.sendFile(path.join(__dirname+'/dist/partiuapp/index.html'));
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/partiu-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
